99bddcf65fab002f358c19402af9486a46932aa4
Thinh Nguyen <thinh.ngtruong@gmail.com>
2022-08-27 01:45:37 +0700

    feat: update tx

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -35,6 +35,9 @@ func TestTransferTx(t *testing.T) {
 		}()
 	}
 
+	// check results
+	existed := make(map[int]bool)
+
 	for i := 0; i < n; i++ {
 		err := <-errs
 		require.NoError(t, err)
@@ -76,12 +79,38 @@ func TestTransferTx(t *testing.T) {
 		require.NoError(t, err)
 
 		// check accounts
-		//fromAccount := result.FromAccount
-		//require.NotEmpty(t, fromAccount)
-		//require.Equal(t, account1.ID, fromAccount.ID)
-		//
-		//toAccount := result.ToAccount
-		//require.NotEmpty(t, toAccount)
-		//require.Equal(t, account2.ID, toAccount.ID)
+		fromAccount := result.FromAccount
+		require.NotEmpty(t, fromAccount)
+		require.Equal(t, account1.ID, fromAccount.ID)
+
+		toAccount := result.ToAccount
+		require.NotEmpty(t, toAccount)
+		require.Equal(t, account2.ID, toAccount.ID)
+
+		// check balances
+		fmt.Println(">> tx:", fromAccount.Balance, toAccount.Balance)
+
+		diff1 := account1.Balance - fromAccount.Balance
+		diff2 := toAccount.Balance - account2.Balance
+		require.Equal(t, diff1, diff2)
+		require.True(t, diff1 > 0)
+		require.True(t, diff1%amount == 0) // 1 * amount, 2 * amount, 3 * amount, ..., n * amount
+
+		k := int(diff1 / amount)
+		require.True(t, k >= 1 && k <= n)
+		require.NotContains(t, existed, k)
+		existed[k] = true
 	}
+
+	// check the final updated balance
+	updatedAccount1, err := store.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+
+	updatedAccount2, err := store.GetAccount(context.Background(), account2.ID)
+	require.NoError(t, err)
+
+	fmt.Println(">> after:", updatedAccount1.Balance, updatedAccount2.Balance)
+
+	require.Equal(t, account1.Balance-int64(n)*amount, updatedAccount1.Balance)
+	require.Equal(t, account2.Balance+int64(n)*amount, updatedAccount2.Balance)
 }
